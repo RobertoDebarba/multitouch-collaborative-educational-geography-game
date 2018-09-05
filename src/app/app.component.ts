9a5d556727274a8b711bd9c7c9eae802139677d9
robertodebarba <roberto.debarba@gmail.com>
2018-09-05 19:49:12 -0300

    Removed Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
 import {Component} from '@angular/core';
-import {Indicator, IndicatorAnimations} from './indicator';
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    animations: IndicatorAnimations
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     eventText = '';
-    indicators;
 
     constructor() {
-        this.indicators = new Indicator();
     }
 
     onPan(evt) {
         this.eventText += `(${evt.center.x}, ${evt.center.y})<br/>`;
-        const indicator = this.indicators.display(
-            evt.center.x,
-            evt.center.y,
-            50
-        );
-        this.indicators.hide(indicator);
     }
 }
