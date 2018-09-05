a5f9fd2688d61f1977ce14696b8fba4f24e25edb
robertodebarba <roberto.debarba@gmail.com>
2018-09-05 19:39:41 -0300

    Implementado exemplo de Pan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,27 @@
 import {Component} from '@angular/core';
+import {Indicator, IndicatorAnimations} from './indicator';
 
 @Component({
-    selector: 'app-root',
+    selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    styleUrls: ['./app.component.css'],
+    animations: IndicatorAnimations
 })
 export class AppComponent {
-    // constant for swipe action: left or right
-    SWIPE_ACTION = {LEFT: 'swipeleft', RIGHT: 'swiperight'};
+    eventText = '';
+    indicators;
 
-    // our list of avatars
-    avatars = [
-        {
-            name: 'kristy',
-            image: 'http://semantic-ui.com/images/avatar2/large/kristy.png',
-            visible: true
-        },
-        {
-            name: 'matthew',
-            image: 'http://semantic-ui.com/images/avatar2/large/matthew.png',
-            visible: false
-        },
-        {
-            name: 'chris',
-            image: 'http://semantic-ui.com/images/avatar/large/chris.jpg',
-            visible: false
-        },
-        {
-            name: 'jenny',
-            image: 'http://semantic-ui.com/images/avatar/large/jenny.jpg',
-            visible: false
-        }
-    ];
+    constructor() {
+        this.indicators = new Indicator();
+    }
 
-    // action triggered when user swipes
-    swipe(currentIndex: number, action = this.SWIPE_ACTION.RIGHT) {
-        // out of range
-        if (currentIndex > this.avatars.length || currentIndex < 0) return;
-
-        let nextIndex = 0;
-
-        // swipe right, next avatar
-        if (action === this.SWIPE_ACTION.RIGHT) {
-            const isLast = currentIndex === this.avatars.length - 1;
-            nextIndex = isLast ? 0 : currentIndex + 1;
-        }
-
-        // swipe left, previous avatar
-        if (action === this.SWIPE_ACTION.LEFT) {
-            const isFirst = currentIndex === 0;
-            nextIndex = isFirst ? this.avatars.length - 1 : currentIndex - 1;
-        }
-
-        // toggle avatar visibility
-        this.avatars.forEach((x, i) => x.visible = (i === nextIndex));
+    onPan(evt) {
+        this.eventText += `(${evt.center.x}, ${evt.center.y})<br/>`;
+        const indicator = this.indicators.display(
+            evt.center.x,
+            evt.center.y,
+            50
+        );
+        this.indicators.hide(indicator);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule, BrowserAnimationsModule, FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
