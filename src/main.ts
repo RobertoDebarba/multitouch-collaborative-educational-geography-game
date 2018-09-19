6dd46072180c0175b0ab1321c21e1aab084d8dff
robertodebarba <roberto.debarba@gmail.com>
2018-09-19 20:10:26 -0300

    Correção build produção

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
-import {environment} from './environments/environment';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 /**
  * Hammerjs must be imported for gestures
@@ -13,5 +13,4 @@ if (environment.production) {
     enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
