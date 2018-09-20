df7bbe0791fb9416eff2ded4b55b24700ea84b67
Guilherme Paz Silva <pazsilvag@gmail.com>
2018-09-19 21:34:36 -0300

    Unity

---
+++ Assets/Init.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour {
+
+    GameObject statesContainer;
+    
+    void Awake()
+    {
+        statesContainer = GameObject.Find("States");
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Close()
+    {
+        Application.Quit();
+    }
+}
